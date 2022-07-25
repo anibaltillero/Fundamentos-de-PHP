9d4534b785f319c3e7e2f3eea3fd47cb834fd19a
ANIBAL GERARDO TILLERO OLARTE <109606202+anibaltillero@users.noreply.github.com>
2022-07-25 09:08:22 -0400

    Add files via upload

--- clases/HTML.php
+++ clases/HTML.php
@@ -68,7 +68,7 @@ class HTML {
 	 */
 	public static function chars($value, $double_encode = TRUE)
 	{
-		return htmlspecialchars( (string) $value, ENT_QUOTES, Configuraciones::$charset, $double_encode);
+		return htmlspecialchars( (string) $value, ENT_QUOTES, Configuracion::$charset, $double_encode);
 	}
 
 	/**
@@ -84,7 +84,7 @@ class HTML {
 	 */
 	public static function entities($value, $double_encode = TRUE)
 	{
-		return htmlentities( (string) $value, ENT_QUOTES, Configuraciones::$charset, $double_encode);
+		return htmlentities( (string) $value, ENT_QUOTES, Configuracion::$charset, $double_encode);
 	}
 
        /**
@@ -249,6 +249,7 @@ class HTML {
 			// Add the attribute key
 			$compiled .= ' '.$key;
 
+                        
 			if ($value OR HTML::$strict)
 			{
 				// Add the attribute value
@@ -256,223 +257,11 @@ class HTML {
 			}
 		}
 
+                
 		return $compiled;
 	}
 
-        
-        
-        
-        
-/**
- * Metodo Inicio de la Tabla y desplegar el encabezado de la pagina web.
- *
- * @access public
- * @since  25/02/2002 05:30:39 p.m.
- **/
-public static function setEncabezado() {
-?>
-     
-   <?php
-   // --------------------------------------------------------------------------
-   //  Trabla Principal del las panatallas
-   //---------------------------------------------------------------------------
-   ?>
-  <table width="<?= Configuraciones::BANNER_ANCHO ?>" align="<?= Configuraciones::BANNER_ALINEAR ?>" height="93%" border="0" cellpadding="0" cellspacing="0" >
- 
-<!--   <tr>
-   <td height="1">
-   <div align="<?= Configuraciones::BANNER_ALINEAR ?>">
-     <img width="<?= Configuraciones::BANNER_ANCHO ?>" height="<?= Configuraciones::BANNER_ALTO_1 ?>" border="0" align="top" src="<?= Configuraciones::IMAGENES_RUTA . 'banner/topbolivariano.jpg' ?>">
-   </div>
-   </td>
-   </tr>-->
 
-   <tr valign="top">
-   <td height="1">
-   <div align="<?= Configuraciones::BANNER_ALINEAR ?>" >
-       <img  width="<?= Configuraciones::BANNER_ANCHO?>" height="<?= Configuraciones::BANNER_ALTO_2 ?>" align="top" src="<?= Configuraciones::IMAGENES_RUTA . 'banner/banner-bottom.png' ?>">
-   </div>
-   </td>
-   </tr>
-<?php 
-}   
-
-        
-  
-
-
-
-/**
-  *   Descripcion: Desplega los titulos de de las pantallas.
-  * 
-  *   Fecha Creacion: 16/04/2015
-  *  
-  *   @example HTML::setTitulos( 'Indica el Titulo' ); 
- * 
-  *   @param   cadena  $titulo  Titulo de la ventana
-  * 
-  */
-public static function setTitulos( $titulo = NULL ) {
-?>
-   
-   
-<tr>
-<td valign="top" class="TITULOS"><?= $titulo ?></td>
-</tr>
-<?php
-}      
-        
-
-
-
-/**
- * Deplegar el titulo.
- *
- * @access private
- * @since 07/07/2014 05:54:45 p.m.
- **/
-public static function setColumnas( $arrColumnas , $intAcciones = 0) {
-?>
-
-
-<
-
-<?php for ($i=0;$i< count( $arrColumnas ) ;$i++) { ?>
-          <th scope="col"><?= $arrColumnas[$i] ?></th>
-<?php } ?>
-
-<?php
-if( $intAcciones >= 1 ) {
-for ($i=0;$i<$intAcciones;$i++) { ?>
-          <th nowrap> &nbsp; </th>
-<?php }  }?>
-
-
-</tr>
-
-<?php
-}
-	
-
-
-
-/** 
- *   Descripcion    : Permite desplegar el pie de pagina del sistema.
- * 
- *  Date created    : 16/04/2015    Date updated: 16/04/2015
- *   @Author        : Anibal G. Tillero O.
- *   @Copyright    : (c) 2018 by MPPRE.
- * 
- *  @example       : HTML::setPieCSS() 
- * -----------------------------------------------------------------------------
- */
- public static function setPieCSS_old() {
-    ?>
-
-    <tr><td>
-    <div class="PIE_TITULO"  align="left">
-        <?= isset( $_SESSION['nombre_usuario'] )? 'USUARIO: '. $_SESSION['nombre_usuario'] : '&nbsp;' ; ?><br>
-    </div>
-    </td></tr>                
-    
-    <tr><td>
-    <div id="PieDePagina" align="center">
-        <?= Configuraciones::EMPRESA ?><br>
-        <?= Configuraciones::INFO_DIRECCION ?><br>
-        <?= Configuraciones::PROYECTO_COPYLEFT ?>
-    </div>
-    </td></tr>
-    
-    <!-- fin de la tabla principal despues del Body -->
-    </table>
-
-
-    <?php    
-}
- 
-
-/** 
- *   Descripcion    : Permite desplegar el pie de pagina del sistema.
- * 
- *  Date created    : 16/04/2015    Date updated: 16/04/2015
- *   @Author        : Anibal G. Tillero O.
- *   @Copyright    : (c) 2018 by MPPRE.
- * 
- *  @example       : HTML::setPieCSS() 
- * -----------------------------------------------------------------------------
- */
- public static function setPieCSS() {
-    ?>
-
-  <tr valign="bottom" >
-    <table width="<?= Configuraciones::BANNER_ANCHO ?>" border="0" align="<?= Configuraciones::BANNER_ALINEAR ?>" cellpadding="0" cellspacing="0" class="formColor">
-        <td width="25%">
-             
-            <div class="PIE_TITULO"  align="left">
-                
-                
-             <?= isset( $_SESSION['nombre_usuario'] )? 'USUARIO: '. $_SESSION['nombre_usuario'] : '&nbsp;' ; ?>
-        
-            </div>
-         </td>   
-         <td width="50%" class="PIE_TITULO"  align="RIGHT">&nbsp;</td>
-         <td width="25%">
-         <div class="PIE_TITULO"  align="RIGHT">
-             <?= isset( $_SESSION['ejercicio'] )? 'EJERCICIO: '. $_SESSION['ejercicio'] : '&nbsp;' ; ?>
-        </div>    
-         
-         </td>        
-                 
-    </table>   
-
-    </tr>                
-                   
-    
-      <tr valign="bottom" >
-  <table width="<?= Configuraciones::BANNER_ANCHO ?>" border="0" align="<?= Configuraciones::BANNER_ALINEAR ?>" cellpadding="0" cellspacing="0" >      
-    <td>
-    <div id="PieDePagina" align="center">
-        <?= Configuraciones::EMPRESA ?><br>
-        <?= Configuraciones::INFO_DIRECCION ?><br>
-        <?= Configuraciones::PROYECTO_COPYLEFT ?>
-    </div>
-    </td>
-   </table>  
-  </tr>
-    
-    <!-- fin de la tabla principal despues del Body -->
-    </table>
-
-
-    <?php    
-}
-        
-        
-        
-
-
-public static function setEspacio($ancho = '92%' )  {
-    
-  ?><td width="<?= $ancho ?>" height="5" nowrap>&nbsp;</td><?php
-
-  
-}
-
-
- public static function MY_Titulos($text = NULL,  array $attributes = NULL) {
-         
-        
-    echo '<h4 '.HTML::attributes($attributes).' >'. strtoupper($text).'</h4>';
-    
-     
-//    echo '<div class="row">';
-//    echo '  <div class="col-12 mt-3 mb-1">';
-//    echo '<h4'.HTML::attributes($attributes).'>'. strtoupper($text).'</h4>';
-//    echo '    <p>' . $subtitulo . '</p>';
-//    echo '  </div>';
-//    echo '</div>';
-    
-   }
         
         
 }
